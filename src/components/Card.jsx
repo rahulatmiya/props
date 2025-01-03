4322413db5d0908cfadb1a57557e86829d3e3e13
rahulatmiya <rahulatmiya@gmail.com>
2025-01-03 16:23:16 +0530

    adding Avatar.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Avatar from "./Avatar"
 
 function Card(props) {
     return <div>
@@ -6,10 +7,7 @@ function Card(props) {
       <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
-          <img className="circle-img"
-            src={props.img}
-            alt="avatar_img"
-          />
+          <Avatar img={props.img}/>
         </div>
         <div className="bottom">
           <p className="info">{props.tel}</p>
