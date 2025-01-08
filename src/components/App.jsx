24f15c8b57bf62c094226d3a9774ac603c9a78ae
rahulatmiya <rahulatmiya@gmail.com>
2025-01-09 00:26:10 +0530

    fixed changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Card from "./Card"
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://th.bing.com/th/id/OIP.6I5zX1BhSeNPDyrKpvibfAAAAA?w=124&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"/>
      <Card 
      name={contacts[0].name}
      img={contacts[0].imgURL}
